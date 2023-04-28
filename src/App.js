a33f724218a89bf403ef03223da4e25fdad909f7
oumaymaRouis <oumaymao690@gmail.com>
2023-04-28 17:40:03 +0100

    auth

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FirstPage from "./main-pages/firstPage";
 import SecondPage from "./main-pages/secondPage";
 import ThirdPage from "./main-pages/thirdPage";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Secret from "./pages/Secret";
 import { ProductsProvider } from "./contexts/ProductsContext";
 
 function App() {
@@ -10,7 +13,10 @@ function App() {
     <ProductsProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<FirstPage />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/" element={<Secret />} />
+          <Route path="/firstpage" element={<FirstPage />} />
           <Route path="/secondpage" element={<SecondPage />} />
           <Route path="/thirdpage" element={<ThirdPage />} />
         </Routes>
---
+++ src/pages/Register.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import axios from "axios";
+import "./auth.css";
+
+export default function Register() {
+  const navigate = useNavigate();
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const generateError = (err) => toast.error(err);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:4000/register",
+        {
+          ...values,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+
+      if (data) {
+        if (data.errors) {
+          const { email, password } = data.errors;
+          if (email) generateError(email);
+          else if (password) generateError(password);
+        } else {
+          navigate("/");
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="auth-main-container">
+      <div className="auth-container">
+        <h2 className="auth-h2">Register Account</h2>
+        <div className="auth-form" onSubmit={(e) => handleSubmit(e)}>
+          <div className="auth-div">
+            <label htmlFor="email">Email</label>
+            <input
+              className="auth-input"
+              type="email"
+              name="email"
+              placeholder="Email"
+              onChange={(e) =>
+                setValues({ ...values, [e.target.name]: e.target.value })
+              }
+            ></input>
+          </div>
+          <div className="auth-div">
+            <label htmlFor="password">Password</label>
+            <input
+              className="auth-input"
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={(e) =>
+                setValues({ ...values, [e.target.name]: e.target.value })
+              }
+            ></input>
+          </div>
+          <button className="auth-btn" type="submit">
+            Submit
+          </button>
+          <span>
+            Already have an account ?<Link to="/login">Login</Link>
+          </span>
+        </div>
+        <ToastContainer />
+      </div>
+    </div>
+  );
+}
