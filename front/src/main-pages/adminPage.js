397a0aab0822393fa3806c0d9e8b755abd225224
oumaymaRouis <oumaymao690@gmail.com>
2023-05-19 14:06:38 +0100

    fix admin css

--- front/src/main-pages/adminPage.js
+++ front/src/main-pages/adminPage.js
@@ -3,7 +3,7 @@ import "../styles/adminPage.css";
 
 function AdminPage() {
   const handleUpdateMe = () => {
-    fetch("http://127.0.0.1:5550/scrape")
+    fetch("http://127.0.0.1:5550/scrap")
       .then((response) => {
         if (response.ok) {
           // Handle successful response
@@ -20,13 +20,14 @@ function AdminPage() {
   };
 
   const handleCheckUsers = () => {
-    window.location.href = "http://localhost:4000/admin";
+    window.location.href = "http://localhost:4000/admin/resources/Users";
   };
 
   return (
     <div className="AdminPage-container">
       <div className="AdminPage-bgr-clr">
         <h1 className="AdminPage-title">Welcome back Admin!</h1>
+
         <p className="AdminPage-paragraph1">So What are we doing today?</p>
         <p className="AdminPage-paragraph2">
           Scraping and updating our DATA or checking up on our users or manage
